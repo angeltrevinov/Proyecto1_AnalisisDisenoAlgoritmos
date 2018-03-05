69fea28369680de85f8c2eccc0694ec4fea35ec8
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-03-05 14:08:42 -0600

    adding main for first problem

---
+++ Proy01A–Median/main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva A01336559
+  Noe Amador Campos Castillo    A00820323
+  Proyecto: 1A  Fecha: 5/Marzo  Materia: Analisis y Diseños de Algoritmos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int Cases; // number of cases
+
+  return 0;
+}
